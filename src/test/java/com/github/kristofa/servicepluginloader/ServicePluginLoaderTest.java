5215ee8d238136c3bf29909a18f6199f910c5991
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2014-02-09 16:08:50 +0100

    Extend test for exact matching properties and add test for no matching properties.

--- src/test/java/com/github/kristofa/servicepluginloader/ServicePluginLoaderTest.java
+++ src/test/java/com/github/kristofa/servicepluginloader/ServicePluginLoaderTest.java
@@ -1,6 +1,7 @@
 package com.github.kristofa.servicepluginloader;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -42,10 +43,23 @@ public class ServicePluginLoaderTest {
     }
 
     @Test
-    public void testGet() {
+    public void testGetExactMatchingProperties() {
         final Properties properties = new Properties();
         properties.setProperty("shape", "plus");
         final Collection<ShapeDrawer> plugins = pluginLoader.get(ShapeDrawer.class, properties);
         assertEquals(1, plugins.size());
+
+        final Properties properties2 = new Properties();
+        properties2.setProperty("shape", "min");
+        final Collection<ShapeDrawer> plugins2 = pluginLoader.get(ShapeDrawer.class, properties2);
+        assertEquals(1, plugins2.size());
+    }
+
+    @Test
+    public void testGetNoMatchingProperties() {
+        final Properties properties = new Properties();
+        properties.setProperty("shape", "hashtag");
+        final Collection<ShapeDrawer> plugins = pluginLoader.get(ShapeDrawer.class, properties);
+        assertTrue(plugins.isEmpty());
     }
 }
