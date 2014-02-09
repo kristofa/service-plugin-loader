4cfdcc95ad607dacf9f80712192dd393bad63d38
Kristof Adriaenssens <kristof.adriaenssens@gmail.com>
2014-02-09 15:57:31 +0100

    Improve ServicePluginsClassPathProvider javadoc.

--- src/main/java/com/github/kristofa/servicepluginloader/ServicePluginsClassPathProvider.java
+++ src/main/java/com/github/kristofa/servicepluginloader/ServicePluginsClassPathProvider.java
@@ -12,7 +12,7 @@ public interface ServicePluginsClassPathProvider {
     /**
      * Gets classpaths for each of the plugins of our application.
      * 
-     * @return Collection which contains 1 entry for each plugin. Each entry contains the classpath for the plugin.
+     * @return Collection which contains 1 entry for each plugin. Each entry contains the classpath for a plugin.
      */
     Collection<ServicePluginClasspath> getPlugins();
 
